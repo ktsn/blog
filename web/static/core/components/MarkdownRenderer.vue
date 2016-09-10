75280ee8832b98f85f7f250a34bdaf22ec121031
ktsn <ktsn55@gmail.com>
2016-09-10 20:09:07 +0900

    Add markdown renderer

---
+++ web/static/core/components/MarkdownRenderer.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.markdonw-renderer(v-html="formatted")
+</template>
+
+<script>
+import marked from 'marked'
+
+import highlight from 'highlight.js'
+import 'highlight.js/styles/github.css'
+
+marked.setOptions({
+  highlight (code) {
+    return highlight.highlightAuto(code).value
+  }
+})
+
+export default {
+  props: {
+    body: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    formatted () {
+      return marked(this.body)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
