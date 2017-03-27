1fafc59759af4b4dba0067079744db9d5ede92f6
ktsn <ktsn55@gmail.com>
2017-03-28 01:12:49 +0900

    Show navigation bar on top of a page

--- web/static/admin/App.vue
+++ web/static/admin/App.vue
@@ -1,7 +1,20 @@
 <template lang="pug">
 .app
-  router-view
+  .app-header
+    navigation
+  .app-main
+    router-view
 </template>
+
+<script>
+import Navigation from './partials/Navigation'
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 </style>
---
+++ web/static/admin/partials/Navigation.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+nav.navigation
+  ul.navigation-list
+    li.natvigation-item
+      a.navigation-link(href="/")
+        span.navigation-link-text Top
+    li.navigation-item
+      store-link.navigation-link(to="new-article")
+        span.navigation-link-text New Article
+</template>
+
+<style lang="scss">
+@import '~core/variables';
+
+.navigation {
+  display: block;
+  background-color: $c-color-base;
+
+  &-list {
+    display: flex;
+  }
+
+  &-link {
+    display: flex;
+    align-items: center;
+    position: relative;
+    padding: 0 25px;
+    height: 40px;
+
+    &::after {
+      content: '';
+      opacity: 0;
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background-color: #fff;
+      transition: opacity $transition-hover;
+    }
+
+    &:hover {
+      text-decoration: none;
+
+      &::after {
+        opacity: 0.2;
+      }
+    }
+
+    &-text {
+      color: #fff;
+    }
+  }
+}
+</style>
